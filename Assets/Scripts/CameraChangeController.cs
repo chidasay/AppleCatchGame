53350a0d4e1d37da0c7676547823b59057e16cc0
chidasay <chidasay@gmail.com>
2026-01-01 20:29:55 +0900

    カメラの切り替えができなくなっていたところを修正したり、コードを少し変えたり

--- Assets/Scripts/CameraChangeController.cs
+++ Assets/Scripts/CameraChangeController.cs
@@ -2,41 +2,38 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+public enum CameraNow
+{
+    MainCamera,
+    SubCamera
+}
+
 public class CameraChangeController : MonoBehaviour
 {
-    private GameObject mainCamera;
-    private GameObject subCamera;
+    [SerializeField] private GameObject mainCamera;
+    [SerializeField] private GameObject subCamera;
 
-    static private int cameraNow = 0; // 0でメイン、1でサブ
-
-
-    // Start is called before the first frame update
+    private CameraNow now = CameraNow.MainCamera;
+    
     void Start()
     {
-        // メインカメラとサブカメラを取得
-        mainCamera = GameObject.Find("MainCamera");
-        subCamera = GameObject.Find("SubCamera");
-
-        // サブカメラを非アクティブ
         subCamera.SetActive(false);
     }
 
     // Update is called once per frame
     void Update()
     {
-        // 右クリックされたらもう一方のカメラをアクティブにする
         if (Input.GetMouseButtonDown(1))
         {
-            if(cameraNow == 0)
+            if(now == CameraNow.MainCamera)
             {
                 mainCamera.SetActive(false);
                 subCamera.SetActive(true);
-                cameraNow = 1;
-            } else if(cameraNow == 1)
-            {
+                now = CameraNow.SubCamera;
+            } else {
                 mainCamera.SetActive(true);
                 subCamera.SetActive(false);
-                cameraNow = 0;
+                now = CameraNow.MainCamera;
             }
         }
     }
