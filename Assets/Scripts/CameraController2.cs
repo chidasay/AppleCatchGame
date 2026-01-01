a6ba5964ddab0bb383fe0eb8097bf490917d6d0d
chidasay <chidasay@gmail.com>
2026-01-01 18:29:30 +0900

    新しいキャラクターに追随するカメラコントローラ

---
+++ Assets/Scripts/CameraController2.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController2 : MonoBehaviour
+{
+    private GameObject player;
+    private Vector3 offset;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.Find("unitychan_remake");
+        offset = transform.position - player.transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = offset + player.transform.position;
+    }
+}
